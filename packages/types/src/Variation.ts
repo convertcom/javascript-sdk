9cb711cdd4af84cb0ced2d191516354105190541
Ahmed Abbas <a.abbas@ixdc.net>
2023-07-28 12:45:55 +0300

    fix: add variation change type

--- packages/types/src/Variation.ts
+++ packages/types/src/Variation.ts
@@ -4,12 +4,23 @@
  * Copyright(c) 2020 Convert Insights, Inc
  * License Apache-2.0
  */
+import {VariationType} from '@convertcom/js-sdk-enums';
 import {Id} from './Id';
+
+export type VariationChange = {
+  id: Id;
+  type: VariationType;
+  data: {
+    feature_id?: Id;
+    variables_data?: Record<string, any>;
+    [key: string]: Id | Record<string, any>;
+  };
+};
 
 export type Variation = {
   id: Id;
   key: string;
   name: string;
-  changes: Array<Record<string, any>>;
+  changes: Array<VariationChange>;
   traffic_allocation?: number;
 };
