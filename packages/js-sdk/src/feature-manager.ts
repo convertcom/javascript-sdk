46edfd478bdd30ec5c4d40da08a77bc6e48d6fcf
Ahmed Abbas <a.abbas@ixdc.net>
2024-03-08 22:09:46 +0200

    remove isPlainObject utility and detect rule data interface explicitly

--- packages/js-sdk/src/feature-manager.ts
+++ packages/js-sdk/src/feature-manager.ts
@@ -26,7 +26,11 @@ import {
   VariationChangeType
 } from '@convertcom/js-sdk-enums';
 
-import {castType, arrayNotEmpty, isPlainObject} from '@convertcom/js-sdk-utils';
+import {
+  castType,
+  arrayNotEmpty,
+  objectNotEmpty
+} from '@convertcom/js-sdk-utils';
 import {BucketedVariation} from '@convertcom/js-sdk-types';
 
 /**
@@ -396,7 +400,7 @@ export class FeatureManager implements FeatureManagerInterface {
             );
           }
 
-          if (typeCasting && isPlainObject(variables)) {
+          if (typeCasting && objectNotEmpty(variables)) {
             // Convert variables values types
             for (const variableName in variables) {
               const variableDefinition = declaredFeatures[
