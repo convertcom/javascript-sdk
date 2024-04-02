f645f1470754d7d279f402dd84c91dc3984ac75e
clllaur <clllaur@users.noreply.github.com>
2024-04-02 15:02:32 +0000

    feat: automated TS Serving API update

--- packages/types/src/config/models/ProjectIntegrationGA4.ts
+++ packages/types/src/config/models/ProjectIntegrationGA4.ts
@@ -4,5 +4,10 @@
 /* eslint-disable */
 import type { IntegrationGA4Base } from './IntegrationGA4Base';
 import type { ProjectGASettingsBase } from './ProjectGASettingsBase';
-export type ProjectIntegrationGA4 = (ProjectGASettingsBase & IntegrationGA4Base);
+export type ProjectIntegrationGA4 = (ProjectGASettingsBase & IntegrationGA4Base & {
+    /**
+     * Boolean indicating whether to wait for the page view event to complete before sending other events.
+     */
+    no_wait_pageview?: boolean;
+});
 
