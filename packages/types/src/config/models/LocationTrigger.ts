925f8f702deaacedcdec25fc86aad4a68f1bf279
abbaseya <abbaseya@users.noreply.github.com>
2024-06-08 14:23:07 +0000

    feat: automated TS Serving API update

---
+++ packages/types/src/config/models/LocationTrigger.ts
@@ -0,0 +1,14 @@
+/* generated using openapi-typescript-codegen -- do no edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+import type { LocationTriggerManualData } from './LocationTriggerManualData';
+import type { LocationTriggerTypes } from './LocationTriggerTypes';
+/**
+ * Object that represents the trigger for a location
+ */
+export type LocationTrigger = {
+    type: LocationTriggerTypes;
+    data?: LocationTriggerManualData;
+};
+
---
+++ packages/types/src/config/models/LocationTriggerManualData.ts
@@ -0,0 +1,14 @@
+/* generated using openapi-typescript-codegen -- do no edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+/**
+ * Describes data structure for "manual" type of location trigger
+ */
+export type LocationTriggerManualData = {
+    /**
+     * Custom javascript code to be applied by this trigger
+     */
+    js: string;
+};
+
