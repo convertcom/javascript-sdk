30a25ce082e8840b68dbe52be9648b88f90cc4d6
Ahmed Abbas <a.abbas@ixdc.net>
2024-06-11 05:28:51 +0300

    adding NestJS demo

---
+++ packages/demo-nestjs/src/main.ts
@@ -0,0 +1,29 @@
+import * as cookieParser from 'cookie-parser';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import ConvertContext from './convert/context'; // [ConvertSDK]
+import ConvertSDK, { ConvertConfig, LogLevel } from '@convertcom/js-sdk'; // [ConvertSDK]
+import DataStore from './convert/datastore';
+
+const PORT = process.env.PORT || 3004;
+
+const dataStore = new DataStore();
+
+const sdkConfig: ConvertConfig = {
+  environment: 'live',
+  sdkKey: '10035569/10034190',
+  dataStore, // optional
+  logger: {
+    logLevel: LogLevel.DEBUG,
+  },
+}; // [ConvertSDK]
+
+const sdkInstance: ConvertSDK = new ConvertSDK(sdkConfig);
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.use(cookieParser());
+  app.use(ConvertContext(sdkInstance, dataStore)); // [ConvertSDK]
+  await app.listen(PORT);
+}
+bootstrap();
