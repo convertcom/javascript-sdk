7b371e59118ba041897f45404a80f871bcb7c588
Ahmed Abbas <a.abbas@ixdc.net>
2024-06-13 18:38:17 +0300

    work in progress

--- packages/api/.prettierrc.js
+++ packages/api/.prettierrc.js
@@ -1,16 +1,16 @@
 // .prettierrc to set prettier settings
 // this file is in JS format to add comments.
 module.exports = {
-    // all strings are saved with singleQuote.
-    singleQuote: true,
-    // tab width 2 spaces
-    tabWidth: 2,
-    // semicolon at the end is true
-    semi: true,
-    // trailing commas where valid in es5 (arrays, objects, etc)
-    trailingComma: 'none',
-    // false - Example: {foo: bar}
-    bracketSpacing: false,
-    // end of line must be *nix compatible
-    endOfLine: 'lf'
+  // all strings are saved with singleQuote.
+  singleQuote: true,
+  // tab width 2 spaces
+  tabWidth: 2,
+  // semicolon at the end is true
+  semi: true,
+  // trailing commas where valid in es5 (arrays, objects, etc)
+  trailingComma: 'none',
+  // false - Example: {foo: bar}
+  bracketSpacing: false,
+  // end of line must be *nix compatible
+  endOfLine: 'lf'
 };
