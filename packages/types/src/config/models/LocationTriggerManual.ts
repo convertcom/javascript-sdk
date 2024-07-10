bc7607742ca284cb74bfb3c111d220472ba0048b
DmytroConvert <DmytroConvert@users.noreply.github.com>
2024-07-10 23:33:05 +0000

    feat: automated TS Serving API update

---
+++ packages/types/src/config/models/LocationTriggerManual.ts
@@ -0,0 +1,14 @@
+/* generated using openapi-typescript-codegen -- do no edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+import type { LocationTriggerBase } from './LocationTriggerBase';
+export type LocationTriggerManual = (LocationTriggerBase & {
+    type?: LocationTriggerManual.type;
+});
+export namespace LocationTriggerManual {
+    export enum type {
+        MANUAL = 'manual',
+    }
+}
+
