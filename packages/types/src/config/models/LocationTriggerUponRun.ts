eea54474f333c33095f679d2b5c396c4cf9be451
DmytroConvert <DmytroConvert@users.noreply.github.com>
2024-07-23 16:28:11 +0000

    feat: automated TS Serving API update

---
+++ packages/types/src/config/models/LocationTriggerUponRun.ts
@@ -0,0 +1,14 @@
+/* generated using openapi-typescript-codegen -- do no edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+import type { LocationTriggerBase } from './LocationTriggerBase';
+export type LocationTriggerUponRun = (LocationTriggerBase & {
+    type?: LocationTriggerUponRun.type;
+});
+export namespace LocationTriggerUponRun {
+    export enum type {
+        UPON_RUN = 'upon_run',
+    }
+}
+
