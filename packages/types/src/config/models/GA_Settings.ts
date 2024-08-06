47e54197c5b29c2b3688cdb9978178cc983bfc21
DmytroConvert <DmytroConvert@users.noreply.github.com>
2024-08-06 22:21:24 +0000

    feat: automated TS Serving API update

--- packages/types/src/config/models/GA_Settings.ts
+++
@@ -1,8 +0,0 @@
-/* generated using openapi-typescript-codegen -- do no edit */
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-import type { ProjectIntegrationGA3 } from './ProjectIntegrationGA3';
-import type { ProjectIntegrationGA4 } from './ProjectIntegrationGA4';
-export type GA_Settings = (ProjectIntegrationGA3 | ProjectIntegrationGA4);
-
