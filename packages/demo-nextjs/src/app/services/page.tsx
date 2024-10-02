7c1b4e2d09d10f14956e8bfc6b41b124d9a6bbe6
joe <joe@joes-MacBook-Pro.local>
2024-10-02 19:27:57 +0300

    adding next js demo

---
+++ packages/demo-nextjs/src/app/services/page.tsx
@@ -0,0 +1,8 @@
+export default function Services() {
+    return (
+      <div className="p-8">
+        <h1 className="text-2xl font-bold">Services Page</h1>
+        <p>This is the services page.</p>
+      </div>
+    );
+  }
