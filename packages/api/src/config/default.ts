eb27ab261cd5a144ca5152aefe4376ac79750b74
Ahmed Abbas <a.abbas@ixdc.net>
2024-11-06 06:21:29 +0200

    fix: environment endpoints

--- packages/api/src/config/default.ts
+++ packages/api/src/config/default.ts
@@ -6,10 +6,11 @@
  */
 
 export const DEFAULT_CONFIG_ENDPOINT =
-  typeof process.env.CONFIG_ENDPOINT === 'string'
+  typeof process.env.CONFIG_ENDPOINT === 'string' &&
+  !!process.env.CONFIG_ENDPOINT
     ? process.env.CONFIG_ENDPOINT
     : 'https://cdn-4.convertexperiments.com/api/v1/';
 export const DEFAULT_TRACK_ENDPOINT =
-  typeof process.env.TRACK_ENDPOINT === 'string'
+  typeof process.env.TRACK_ENDPOINT === 'string' && !!process.env.TRACK_ENDPOINT
     ? process.env.TRACK_ENDPOINT
     : 'https://[project_id].metrics.convertexperiments.com/v1/';
