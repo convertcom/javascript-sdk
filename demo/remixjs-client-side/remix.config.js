4cbf9a8212d2e60ccf4f2cc24e4c0da86717ee07
Joseph Samir <joseph.samir@msa.edu.eg>
2024-11-18 21:48:23 +0200

    feat: adding remix-js-client-side demo

---
+++ demo/remixjs-client-side/remix.config.js
@@ -0,0 +1,16 @@
+/** @type {import('@remix-run/dev').AppConfig} */
+export default {
+  ignoredRouteFiles: ["**/.*"],
+  // appDirectory: "app",
+  // assetsBuildDirectory: "public/build",
+  // publicPath: "/build/",
+  // serverBuildPath: "build/index.js",
+  // browserNodeBuiltinsPolyfill: {
+  //   modules: {
+  //     url: true,
+  //     http: true,
+  //     https: true,
+  //     querystring: true,
+  //   },
+  // },
+};
